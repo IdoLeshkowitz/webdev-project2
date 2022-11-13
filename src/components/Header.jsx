42bca7323f1034693a9367eec9973bf600603921
IdoLeshkowitz <idoleshkowitz@gmail.com>
2022-11-13 08:36:02 +0200

    v3.4

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,6 +4,8 @@ function Header() {
   return (
     <header className="page-header-box">
          <h1>IDO</h1><AiOutlineGitlab className="header-logo"></AiOutlineGitlab>
+         <input type='checkbox' id='header-theme-selector' className="header-theme-selector"></input>
+         <label for='header-theme-selector'>theme </label>
     </header>
   );
 }
